27c2e026145656b13b3d9d314be949e210cd6d44
MarlenaFlueh <mf@esyoil.com>
2018-04-17 16:03:36 +0200

    refactor: move boards state to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, Switch, withRouter} from 'react-router-dom';
 
+import Layout from './hoc/Layout/Layout';
 import Board from './container/Board/Board';
 import BoardOverview from './container/BoardOverview/BoardOverview';
 
-const app = () => (
-  <Switch>
-    <Route path="/board/:id" component={Board} />
-    <Route path="/" component={BoardOverview} />
-  </Switch>
-);
+class App extends Component {
+  state = {
+    boards: null
+  };
 
-export default app;
+  async componentDidMount() {
+    const response = await fetch('https://prollo-8a5a5.firebaseio.com/boards.json');
+    const boards = await response.json();
+    const updatedBoards = [];
+
+    for (let key in boards) {
+      updatedBoards.push({
+        id: key,
+        ...boards[key]
+      });
+    }
+
+    this.setState({boards: updatedBoards});
+  };
+
+  onCreate = async title => {
+    const oldBoards = [...this.state.boards];
+    const response = await fetch('https://prollo-8a5a5.firebaseio.com/boards.json', {
+      method: 'post',
+      body: JSON.stringify({title})
+    });
+
+    const jsonResponse = await response.json();
+    const board = {
+      id: jsonResponse.name,
+      title
+    };
+
+    const boards = [
+      ...oldBoards,
+      board
+    ];
+
+    this.setState({boards});
+    this.props.history.push(`/board/${board.id}`);
+  };
+
+  render() {
+    let boards = <p>Loading...</p>;
+
+    if (this.state.boards) {
+      boards = (
+        <Layout boards={this.state.boards}>
+          <Switch>
+            <Route path="/board/:id" component={Board} />
+            <Route 
+              path="/" 
+              render={() => <BoardOverview boards={this.state.boards} 
+              onCreate={this.onCreate} />} 
+            />
+          </Switch>
+        </Layout>
+      );
+    }
+
+    return boards;
+  }
+}
+
+export default withRouter(App);
--- src/container/BoardOverview/BoardOverview.js
+++ src/container/BoardOverview/BoardOverview.js
@@ -3,71 +3,21 @@ import React, {Component} from 'react';
 import CreateBoard from '../../components/CreateBoard/CreateBoard';
 import Boards from '../../components/Boards/Boards';
 import CreateBoardModal from '../../components/CreateBoardModal/CreateBoardModal';
-import Layout from '../../hoc/Layout/Layout';
 
 class BoardOverview extends Component {
-  state = {
-    boards: null
-  };
-
-  async componentDidMount() {
-    const response = await fetch('https://prollo-8a5a5.firebaseio.com/boards.json');
-    const boards = await response.json();
-    const updatedBoards = [];
-
-    for (let key in boards) {
-      updatedBoards.push({
-        id: key,
-        ...boards[key]
-      });
-    }
-
-    this.setState({boards: updatedBoards});
-  };
-
-  onCreate = async title => {
-    const oldBoards = [...this.state.boards];
-    const response = await fetch('https://prollo-8a5a5.firebaseio.com/boards.json', {
-      method: 'post',
-      body: JSON.stringify({title})
-    });
-
-    const jsonResponse = await response.json();
-    const board = {
-      id: jsonResponse.name,
-      title
-    };
-
-    const boards = [
-      ...oldBoards,
-      board
-    ];
-
-    this.setState({boards});
-    this.props.history.push(`/board/${board.id}`);
-  };
-
   render() {
-    let boards = <p>Loading...</p>;
-
-    if (this.state.boards) {
-      boards = (
-        <Layout boards={this.state.boards}>
-          <div className="container">
-            <h1 className="my-5">Personal Boards</h1>
-            <div className="row">
-              <Boards boards={this.state.boards} />
-              <div className="col-4 mb-4">
-                <CreateBoard />
-              </div>
-            </div>
-            <CreateBoardModal clicked={this.onCreate} />
+    return (
+      <div className="container">
+        <h1 className="my-5">Personal Boards</h1>
+        <div className="row">
+          <Boards boards={this.props.boards} />
+          <div className="col-4 mb-4">
+            <CreateBoard />
           </div>
-        </Layout>
-      );
-    }
-
-    return boards;
+        </div>
+        <CreateBoardModal clicked={this.props.onCreate} />
+      </div>
+    );
   }
 }
 
