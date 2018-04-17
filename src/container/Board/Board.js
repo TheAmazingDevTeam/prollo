18ec9bfa85d4b4d64e791a92cc087ef7129f5f46
MarlenaFlueh <mf@esyoil.com>
2018-04-17 16:25:05 +0200

    add: markup to Board.js

--- src/container/Board/Board.js
+++ src/container/Board/Board.js
@@ -3,8 +3,41 @@ import React, {Component} from 'react';
 class Board extends Component {
   render() {
     return (
-      <div>
-        <h1>actual board</h1>
+      <div className="container-fluid">
+        <h1>Board title</h1>
+        <div className="row">
+          <div className="col-2">
+            <h3>List 1</h3>
+            <div className="card">
+              <div className="card-body">
+                <h5>Card Title</h5>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h5>Card Title</h5>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h5>Card Title</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-2">
+            <h3>List 2</h3>
+            <div className="card">
+              <div className="card-body">
+                <h5>Card Title</h5>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h5>Card Title</h5>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
