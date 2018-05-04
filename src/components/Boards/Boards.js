23414e2eec32bd1024ab3337b2de817baf9dee12
MarlenaFlueh <mf@esyoil.com>
2018-05-04 11:11:30 +0200

    remove onclick event on board

--- src/components/Boards/Boards.js
+++ src/components/Boards/Boards.js
@@ -5,12 +5,7 @@ import BoardSelection from '../../components/BoardSelection/BoardSelection';
 
 const boards = props =>
   props.boards.map(board => (
-    <Col
-      xs="4"
-      key={board.id}
-      onClick={() => props.clicked(board)}
-      className="mb-4"
-    >
+    <Col xs="4" key={board.id} className="mb-4">
       <BoardSelection title={board.title} id={board.id} />
     </Col>
   ));
