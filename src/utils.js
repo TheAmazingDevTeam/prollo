4bf1743f5337ff1bfbd56edeac65546a63543c79
Liam <lw@esyoil.com>
2018-05-05 13:33:30 +0200

    comment on utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Convert an object to an array with an id
+ * @param {object} object - The object which should be converted
+ * @returns {Array} - New array
+ */
 export const mapObjectToArray = object =>
   object !== null
     ? Object.keys(object).map(key => ({
