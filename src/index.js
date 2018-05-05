885d9b9c82670fd35736a3353fcd3416f22488fc
Liam <lw@esyoil.com>
2018-05-05 13:09:15 +0200

    document index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/**
+ * Wrap App Component with router
+ */
 const app = (
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
 
+/**
+ * Mount App Component to DOM
+ */
 ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
